c51282c78126bb4b7f9758921dfc7942623aa15d
Nick <nfears5798@gmail.com>
2023-09-12 23:59:36 -0400

    final commit for day

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+// src/components/HomePage.js
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Blog Site</h1>
+      <p>Explore and share your thoughts with the world!</p>
+    </div>
+  );
+};
+
+export default HomePage;
