f01b29d33b0b9c92b5c2a6748a8ee7b15f53c424
Nick <nfears5798@gmail.com>
2023-09-13 15:34:43 -0400

    added stuff

--- src/Categories.js
+++ src/Categories.js
@@ -179,34 +179,39 @@ const Categories = () => {
         <button className="category-btn" onClick={toggleMisc}>
           Misc
         </button>
-        <button className="category-btn">Other</button>
         {showMLBTeams && (
         <div className="team-list">
+          <h3>American League East</h3>
           <button className="team-btn">New York Yankees</button>
           <button className="team-btn">Boston Red Sox</button>
           <button className="team-btn">Baltimore Orioles</button>
           <button className="team-btn">Toronto Blue Jays</button>
           <button className="team-btn">Tampa Bay Rays</button>
+          <h3>American League Central</h3>
           <button className="team-btn">Minnesota Twins</button>
           <button className="team-btn">Kansas City Royals</button>
           <button className="team-btn">Detroit Tigers</button>
           <button className="team-btn">Cleveland Guardians</button>
           <button className="team-btn">Chicago White Sox</button>
+          <h3>American League West</h3>
           <button className="team-btn">Houston Astros</button>
           <button className="team-btn">Texas Rangers</button>
           <button className="team-btn">Oakland A's</button>
           <button className="team-btn">Los Angeles Angels</button>
           <button className="team-btn">Seattle Mariners</button>
+          <h3>National League East</h3>
           <button className="team-btn">New York Mets</button>
           <button className="team-btn">Atlanta Braves</button>
           <button className="team-btn">Philadelphia Phillies</button>
           <button className="team-btn">Miami Marlins</button>
           <button className="team-btn">Washington Nationals</button>
+          <h3>National League Central</h3>
           <button className="team-btn">Chicago Cubs</button>
           <button className="team-btn">St. Louis Cardinals</button>
           <button className="team-btn">Cincinnati Reds</button>
           <button className="team-btn">Milwaukee Brewers</button>
           <button className="team-btn">Pittsburgh Pirates</button>
+          <h3>National League West</h3>
           <button className="team-btn">Los Angeles Dodgers</button>
           <button className="team-btn">Arizona Diamondbacks</button>
           <button className="team-btn">Colorado Rockies</button>
@@ -220,9 +225,42 @@ const Categories = () => {
 
         {showNBATeams && (
         <div className="team-list">
-          <button className="team-btn">Team A</button>
-          <button className="team-btn">Team B</button>
-          <button className="team-btn">Team C</button>
+          <h3>Atlantic</h3>
+          <button className="team-btn">New York Knicks</button>
+          <button className="team-btn">Boston Celtics</button>
+          <button className="team-btn">Brooklyn Nets</button>
+          <button className="team-btn">Toronto Raptors</button>
+          <button className="team-btn">Philadelphia 76ers</button>
+          <h3>Central</h3>
+          <button className="team-btn">Chicago Bulls</button>
+          <button className="team-btn">Cleveland Cavaliers</button>
+          <button className="team-btn">Detroit Pistons</button>
+          <button className="team-btn">Milwaukee Bucks</button>
+          <button className="team-btn">Indiana Pacers</button>
+          <h3>Southeast</h3>
+          <button className="team-btn">Atlanta Hawks</button>
+          <button className="team-btn">Miami Heat</button>
+          <button className="team-btn">Orlando Magic</button>
+          <button className="team-btn">Charlotte Hornets</button>
+          <button className="team-btn">Washinton Wizards</button>
+          <h3>Northwest</h3>
+          <button className="team-btn">Denver Nuggets</button>
+          <button className="team-btn">Minnesota Timberwolves</button>
+          <button className="team-btn">Oklahoma City Thunder</button>
+          <button className="team-btn">Portland Trail Blazers</button>
+          <button className="team-btn">Utah Jazz</button>
+          <h3>Pacific</h3>
+          <button className="team-btn">Sacramento Kings</button>
+          <button className="team-btn">Golden State Warrios</button>
+          <button className="team-btn">Los Angeles Clippers</button>
+          <button className="team-btn">Los Angeles Lakers</button>
+          <button className="team-btn">Phoenix Suns</button>
+          <h3>Southwest</h3>
+          <button className="team-btn">Memphis Grizzlies</button>
+          <button className="team-btn">New Orleans Pelicans</button>
+          <button className="team-btn">Dallas Mavericks</button>
+          <button className="team-btn">Houston Rockets</button>
+          <button className="team-btn">San Antonio Spurs</button>
           {/* Add more NBA teams here */}
         </div>
         )}
@@ -231,9 +269,46 @@ const Categories = () => {
    
         {showNFLTeams && (
         <div className="team-list">
-          <button className="team-btn">Team X</button>
-          <button className="team-btn">Team Y</button>
-          <button className="team-btn">Team Z</button>
+          <h3>AFC East</h3>
+          <button className="team-btn">New York Jets</button>
+          <button className="team-btn">New England Patriots</button>
+          <button className="team-btn">Buffalo Bills</button>
+          <button className="team-btn">Miami Dolphins</button>
+          <h3>AFC North</h3>
+          <button className="team-btn">Cleveland Browns</button>
+          <button className="team-btn">Cincinnati Bengals</button>
+          <button className="team-btn">Pittsburgh Steelers</button>
+          <button className="team-btn">Baltimore Ravens</button>
+          <h3>AFC South</h3>
+          <button className="team-btn">Jacksonville Jaguars</button>
+          <button className="team-btn">Indianapolis Colts</button>
+          <button className="team-btn">Tennessee Titans</button>
+          <button className="team-btn">Houston Texans</button>
+          <h3>AFC West</h3>
+          <button className="team-btn">Las Vegas Raiders</button>
+          <button className="team-btn">Kansas City Chiefs</button>
+          <button className="team-btn">Los Angeles Chargers</button>
+          <button className="team-btn">Denver Broncos</button>
+          <h3>NFC East</h3>
+          <button className="team-btn">New York Giants</button>
+          <button className="team-btn">Philadelphia Eagles</button>
+          <button className="team-btn">Dallas Cowboys</button>
+          <button className="team-btn">Washinton Commanders</button>
+          <h3>NFC North</h3>
+          <button className="team-btn">Green Bay Packers</button>
+          <button className="team-btn">Chicago Bears</button>
+          <button className="team-btn">Detroit Lions</button>
+          <button className="team-btn">Minnesota Vikings</button>
+          <h3>NFC South</h3>
+          <button className="team-btn">Atlanta Falcons</button>
+          <button className="team-btn">Carolina Panthers</button>
+          <button className="team-btn">Tampa Bay Buccaneers</button>
+          <button className="team-btn">New Orleans Saints</button>
+          <h3>NFC West</h3>
+          <button className="team-btn">Arizona Carindals</button>
+          <button className="team-btn">San Francisco 49ers</button>
+          <button className="team-btn">Los Angeles Rams</button>
+          <button className="team-btn">Seattle Seahawks</button>
           {/* Add more NFL teams here */}
         </div>
         )}
@@ -241,18 +316,63 @@ const Categories = () => {
       <div className='category-list'>
         {showCollegeFootball && (
           <div className="team-list">
-          <button className="team-btn">Team X</button>
-          <button className="team-btn">Team Y</button>
-          <button className="team-btn">Team Z</button>
+          <button className="team-btn">ACC</button>
+          <button className="team-btn">SEC</button>
+          <button className="team-btn">Big 12</button>
+          <button className="team-btn">Big 10</button>
+          <button className="team-btn">Pac 12</button>
         </div>
             )}
       </div>
       <div className='category-list'>
         {showCollegeBasketball && (
           <div className="team-list">
-          <button className="team-btn">Team X</button>
-          <button className="team-btn">Team Y</button>
-          <button className="team-btn">Team Z</button>
+          <button className="team-btn">ACC</button>
+          <button className="team-btn">SEC</button>
+          <button className="team-btn">Big 12</button>
+          <button className="team-btn">Big 10</button>
+          <button className="team-btn">Pac 12</button>
+          <button className="team-btn">AAC</button>
+          <button className="team-btn">Mountain West</button>
+        </div>
+            )}
+      </div>
+      <div className='category-list'>
+        {showMusic && (
+          <div className="team-list">
+          <button className="team-btn">Pop</button>
+          <button className="team-btn">Hip/Hop</button>
+          <button className="team-btn">R&B</button>
+          <button className="team-btn">Country</button>
+          <button className="team-btn">Rock</button>
+          <button className="team-btn">Alternative</button>
+          <button className="team-btn">Indie</button>
+          <button className="team-btn">Folk</button>
+        </div>
+            )}
+      </div>
+      <div className='category-list'>
+        {showMovies && (
+          <div className="team-list">
+          <button className="team-btn">Action</button>
+          <button className="team-btn">Thriller</button>
+          <button className="team-btn">Drama</button>
+          <button className="team-btn">Comedy</button>
+          <button className="team-btn">Romantic Comedy</button>
+          <button className="team-btn">Horror</button>
+          <button className="team-btn">Biopics</button>
+          <button className="team-btn">Sci-Fi</button>
+          <button className="team-btn">Sports</button>
+          <button className="team-btn">Animated</button>
+        </div>
+            )}
+      </div>
+      <div className='category-list'>
+        {showTV && (
+          <div className="team-list">
+          <button className="team-btn">Comedy</button>
+          <button className="team-btn">Drama</button>
+          <button className="team-btn">Docuseries</button>
         </div>
             )}
       </div>
